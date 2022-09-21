d350284878f715ab89b7b0a3b2219d184fcb7fa2
development0261 <development0261@gmail.com>
2022-09-21 16:05:41 +0530

    better implement"

--- webscrap_main/scrap.py
+++ webscrap_main/scrap.py
@@ -40,8 +40,8 @@ def scrape(country_name,search_keyword):
     # chrome_options.binary_location = GOOGLE_CHROME_PATH
     # # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
     # driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=chrome_options)
     
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
