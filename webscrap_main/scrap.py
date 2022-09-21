41285644649c0070d971caedbb59fe1c6b382232
development0261 <development0261@gmail.com>
2022-09-21 11:22:40 +0530

    better implement"

--- webscrap_main/scrap.py
+++ webscrap_main/scrap.py
@@ -42,7 +42,7 @@ def scrape(country_name,search_keyword):
     # chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=chrome_options)
     
     # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     website = 'https://www.facebook.com/ads/library/'
