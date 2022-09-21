cf1e9c93f37ba4fd19b5fc21dd6066bcaa89d7b6
development0261 <development0261@gmail.com>
2022-09-21 15:53:28 +0530

    better implement"

--- webscrap_main/scrap.py
+++ webscrap_main/scrap.py
@@ -46,7 +46,7 @@ def scrape(country_name,search_keyword):
     
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
     print("+________+_+_+_+_+_+_+_+_+_+_+_+",driver)
-    website = 'https://www.facebook.com/ads/library/?active_status=all&ad_type=political_and_issue_ads&country=IN&media_type=all'
+    website = 'https://www.nituk.ac.in/'
     # print("++++++++++++++",platform)
     # if platform =="win32":
     #     path = 'chromedriver.exe' 
